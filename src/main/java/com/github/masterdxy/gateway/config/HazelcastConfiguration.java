97cfb2a6838decc3a7cf49dc9975ea99b5a790d5
masterdxy <masterdxy@outlook.com>
2019-07-15 15:01:28 +0800

    add hazelcast redis configuration.

---
+++ src/main/java/com/github/masterdxy/gateway/config/HazelcastConfiguration.java
@@ -0,0 +1,22 @@
+package com.github.masterdxy.gateway.config;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HazelcastConfiguration {
+
+    @Bean
+    public Config hazelCastConfig() {
+        return new Config()
+                .setInstanceName("gateway-hazelcast-instance");
+    }
+
+    @Bean
+    public HazelcastInstance hazelcastInstance(Config config){
+        return Hazelcast.getOrCreateHazelcastInstance(config);
+    }
+}
--- src/main/java/com/github/masterdxy/gateway/config/RedisConfiguration.java
+++ src/main/java/com/github/masterdxy/gateway/config/RedisConfiguration.java
@@ -1,7 +1,18 @@
 package com.github.masterdxy.gateway.config;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import io.lettuce.core.RedisClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfiguration {
+
+    @NacosValue("${gateway.dubbo.metadata}")
+    private String redisUrl;
+
+    @Bean
+    public RedisClient redisClient(){
+        return RedisClient.create(redisUrl);
+    }
 }
