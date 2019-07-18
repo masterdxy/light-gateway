8b663c78a00848f17ae8b4dab4185dc56cdadc3f
masterdxy <masterdxy@outlook.com>
2019-07-18 17:47:53 +0800

    update plugin handler.

--- src/main/java/com/github/masterdxy/gateway/handler/plugin/PluginHandler.java
+++ src/main/java/com/github/masterdxy/gateway/handler/plugin/PluginHandler.java
@@ -1,8 +1,8 @@
 package com.github.masterdxy.gateway.handler.plugin;
 
 import com.alibaba.fastjson.JSON;
-import com.github.masterdxy.gateway.common.Constant;
 import com.github.masterdxy.gateway.plugin.PluginChain;
+import com.github.masterdxy.gateway.plugin.PluginResult;
 import com.google.gson.JsonObject;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -26,30 +26,35 @@ public class PluginHandler implements Handler<RoutingContext> {
         PluginChain chain = PluginChain.build(context);
         context.
                 vertx().
-                executeBlocking((Handler<Future<Boolean>>) future -> {
-                            logger.info("Running blocking code [chain.execute()]");
-                            boolean result = chain.execute();
-                            future.complete(result);
+                executeBlocking((Handler<Future<PluginResult>>) future -> {
+                            try {
+                                PluginResult result = chain.execute();
+                                future.complete(result);
+                            } catch (Exception e) {
+                                if (e instanceof IllegalStateException){
+                                    logger.error("execute plugin chain error cause no return data after plugin " +
+                                            "executed.", e);
+                                }
+                                logger.error("execute plugin chain error", e);
+                                future.fail(e);
+                            }
                         },
                         asyncResult -> {
-                            logger.info("Finish running blocking code [chain.execute()] ,success :{}",
-                                    asyncResult.succeeded());
+                            PluginResult result = asyncResult.result();
+                            logger.info("finish running plugin chain, success :{}", !result.isError());
                             if (!asyncResult.succeeded()) {
-                                logger.error("PluginChain execute failed, cause : {}", asyncResult.cause());
+                                logger.error("pluginChain execute error, cause : {}", asyncResult.cause());
                                 context.fail(500, asyncResult.cause());
                             } else {
-                                if (asyncResult.result()) {
-                                    JsonObject jsonObject = new JsonObject();
-                                    Object pluginResult = context.get(Constant.PLUGIN_RESULT_KEY);
-                                    if (pluginResult != null) {
-                                        jsonObject.addProperty("result", JSON.toJSONString(pluginResult));
-                                    }
-                                    context.response().setStatusCode(200).end(jsonObject.toString());
+                                if (asyncResult.result().isError()) {
+                                    logger.error("pluginChain execute error, msg : {}",
+                                            result.getErrorMsg());
+                                    context.response().setStatusCode(400).end(result.getErrorMsg());
                                 } else {
-                                    String pluginResult = context.get(Constant.PLUGIN_ERROR_MESSAGE_KEY);
-                                    logger.error("PluginChain execute success, but return false, msg : {}",
-                                            pluginResult);
-                                    context.fail(400, new RuntimeException(pluginResult));
+                                    JsonObject jsonObject = new JsonObject();
+                                    Object pluginResult = result.getData();
+                                    jsonObject.addProperty("result", JSON.toJSONString(pluginResult));
+                                    context.response().setStatusCode(200).end(jsonObject.toString());
                                 }
                             }
                         });
