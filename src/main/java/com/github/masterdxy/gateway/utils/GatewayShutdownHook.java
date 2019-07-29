b8af949ac347e25a5464d4274a7af1bcb927c185
masterdxy <masterdxy@outlook.com>
2019-07-29 16:20:53 +0800

    add shutdown hook.

--- src/main/java/com/github/masterdxy/gateway/utils/GatewayShutdownHook.java
+++ src/main/java/com/github/masterdxy/gateway/utils/GatewayShutdownHook.java
@@ -3,10 +3,12 @@ package com.github.masterdxy.gateway.utils;
 
 import com.github.masterdxy.gateway.config.VertxInitialization;
 import com.github.masterdxy.gateway.spring.SpringContext;
+import com.github.masterdxy.gateway.task.TaskRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class GatewayShutdownHook extends Thread {
 
@@ -16,21 +18,25 @@ public class GatewayShutdownHook extends Thread {
 
     @Override
     public void run() {
-        VertxInitialization.setStarted(false);
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        if (VertxInitialization.getDeploymentIds().size() != 0 && VertxInitialization.isStarted()) {
-            VertxInitialization.getDeploymentIds().forEach(deploymentId ->
-                    VertxInitialization.getVertx().undeploy(deploymentId, async -> {
-                        countDownLatch.countDown();
-                    }));
+        if (VertxInitialization.getDeploymentIds().size() > 0) {
+            if (VertxInitialization.isStarted()) {
+                CountDownLatch countDownLatch = new CountDownLatch(VertxInitialization.getDeploymentIds().size());
+                VertxInitialization.setStarted(false);
+                VertxInitialization.getDeploymentIds().forEach(deploymentId ->
+                        VertxInitialization.getVertx().undeploy(deploymentId, async -> {
+                            countDownLatch.countDown();
+                        }));
+                try {
+                    countDownLatch.await(1000, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException ignore) {
+                }
+                logger.info("Vertx stopped.");
+                TaskRegistry.stopAll();
+                logger.info("Tasks stopped.");
+                SpringContext.stop();
+                logger.info("StringContext stopped.");
+            }
         }
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException ignore) {
-        }
-        logger.info("Vertx stopped.");
-        SpringContext.stop();
-        logger.info("StringContext stopped.");
     }
 
     private GatewayShutdownHook(String name) {
