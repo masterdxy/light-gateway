b4fcf369c034c4fa97cb9aff902c1f5d33ef0b04
masterdxy <masterdxy@outlook.com>
2019-08-12 14:59:06 +0800

    modify spring util

--- src/main/java/com/github/masterdxy/gateway/spring/SpringContext.java
+++ src/main/java/com/github/masterdxy/gateway/spring/SpringContext.java
@@ -3,9 +3,11 @@ package com.github.masterdxy.gateway.spring;
 import com.google.common.collect.Lists;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class SpringContext {
 
@@ -16,13 +18,17 @@ public class SpringContext {
         return context;
     }
 
+    public static void stop() {
+        Objects.requireNonNull((AbstractApplicationContext) context).close();
+    }
+
     public static <T> T instance(Class<T> tClass) {
-        return context.getBean(tClass);
+        return Objects.requireNonNull(context).getBean(tClass);
     }
 
 
     public static <T> List<T> instances(Class<T> iCls) {
-        Map<String, T> maps = context.getBeansOfType(iCls);
+        Map<String, T> maps = Objects.requireNonNull(context).getBeansOfType(iCls);
         if (maps == null || maps.isEmpty()) {
             return Lists.newArrayList();
         }
