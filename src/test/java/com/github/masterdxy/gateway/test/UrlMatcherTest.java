99e3d603fde83648edf1da742d8d24a6a86ab536
dongxinyu <lucas@jm.energy>
2019-10-30 16:21:16 +0800

    add url matcher tests.

---
+++ src/test/java/com/github/masterdxy/gateway/test/UrlMatcherTest.java
@@ -0,0 +1,14 @@
+package com.github.masterdxy.gateway.test;
+
+import com.github.masterdxy.gateway.plugin.endpoint.AntPathMatcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UrlMatcherTest {
+
+    @Test
+    void testMatch() {
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        Assertions.assertTrue(antPathMatcher.match("/crud/**","/crud/user/findOne"));
+    }
+}
